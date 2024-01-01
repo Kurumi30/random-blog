55275148aeed6ed63aa2c2b99e9622c70d56732c
Kurumi30 <bsd3su@gmail.com>
2024-01-01 14:49:59 -0300

    Refactor article controller and update index page

--- controllers/articles.js
+++ controllers/articles.js
@@ -2,46 +2,46 @@ const Article = require('../models/article')
 
 class ArticleController {
   async main(req, res) {
-  const articles = await Article.find().sort({ createdAt: "desc" })
+    const articles = await Article.find().sort({ createdAt: "desc" })
 
-  res.render("articles/index", { articles })
-}
+    return res.render("articles/index", { articles })
+  }
 
   async newArticle(req, res) {
-  res.render("articles/new", { article: new Article() })
-}
+    return res.render("articles/new", { article: new Article() })
+  }
 
   async editArticle(req, res) {
-  const article = await Article.findById(req.params.id)
+    const article = await Article.findById(req.params.id)
 
-  res.render("articles/edit", { article })
-}
+    return res.render("articles/edit", { article })
+  }
 
   async showArticle(req, res) {
-  const article = await Article.findOne({ slug: req.params.slug })
+    const article = await Article.findOne({ slug: req.params.slug })
 
-  if (article == null) res.redirect("/")
+    if (article == null) res.redirect("/")
 
-  res.render("articles/show", { article })
-}
+    return res.render("articles/show", { article })
+  }
 
   async postArticle(req, res, next) {
-  req.article = new Article()
+    req.article = new Article()
 
-  next()
-}
+    next()
+  }
 
   async putArticle(req, res, next) {
-  req.article = await Article.findById(req.params.id)
+    req.article = await Article.findById(req.params.id)
 
-  next()
-}
+    next()
+  }
 
   async deleteArticle(req, res) {
-  await Article.findByIdAndDelete(req.params.id)
+    await Article.findByIdAndDelete(req.params.id)
 
-  res.redirect("/")
-}
+    return res.redirect("/")
+  }
 }
 
 module.exports = ArticleController

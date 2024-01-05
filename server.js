160afc9c773ef5559ea7b324c72d4f5fa23fafdf
Kurumi30 <bsd3su@gmail.com>
2024-01-05 13:03:17 -0300

    Remove unused import statement

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const methodOverride = require('method-override')
-const Article = require('./models/article')
 const articleRouter = require('./routes/articles')
 const ArticleController = require('./controllers/articles')
 
